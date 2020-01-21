71388549bdc48819ad662119555243df54185ed8
Stephen Callum <stephen.callum@hotmail.co.uk>
2020-01-21 23:16:21 +0000

    Trying movement inputs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("a"))
+        {
+            print("A/Left keypressed");
+            anim.Play("WalkLeft", -1, 0f);
+        }
     }
 }
